349a6515ab9655bb3e6882f9c8d1c4442d90d496
Shin-sibainu <shincode0712@gmail.com>
2022-03-25 10:36:43 +0900

    done

--- src/App.js
+++ src/App.js
@@ -1,24 +1,55 @@
-import logo from './logo.svg';
-import './App.css';
+import "./App.css";
+import { Canvas, useFrame } from "@react-three/fiber";
+import { useRef, useState } from "react";
+import { config, useSpring, animated } from "@react-spring/three";
+
+function Box(props) {
+  const ref = useRef();
+
+  const [clicked, setClicked] = useState(false);
+  const [hoverd, setHoverd] = useState(false);
+
+  useFrame(() => (ref.current.rotation.x += 0.01));
+
+  const { scale } = useSpring({
+    scale: clicked ? 2 : 1,
+    config: config.wobbly,
+  });
+
+  return (
+    <animated.mesh
+      {...props}
+      ref={ref}
+      // scale={clicked ? 2 : 1}
+      scale={scale}
+      onClick={() => setClicked(!clicked)}
+      onPointerOver={() => setHoverd(true)}
+      onPointerOut={() => setHoverd(false)}
+    >
+      <boxGeometry args={[1, 1, 1]} />
+      <meshStandardMaterial color={hoverd ? "hotpink" : "orange"} />
+    </animated.mesh>
+  );
+}
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <>
+      <div id="canvas-container">
+        <Canvas>
+          {/* <mesh position={[-10, -2, 1]}> */}
+          {/* <boxGeometry args={[2, 2, 2]} /> */}
+          <Box position={[-1.6, 0, 0]} />
+          <Box position={[1.6, 0, 0]} />
+          <ambientLight intensity={0.5} />
+          <spotLight position={[10, 10, 10]} angle={0.15} penumbra={1} />
+          <pointLight position={[-10, -10, -10]} />
+          {/* </mesh> */}
+        </Canvas>
+      </div>
+      <h1>Theejs Fiber</h1>
+      <a>もっと見る</a>
+    </>
   );
 }
 
